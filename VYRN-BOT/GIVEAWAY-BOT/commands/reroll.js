a15ee6d23eefa218678a6d32655177c82dbcac5a
B3sttieeee <andrzejbraun67@gmail.com>
2026-03-25 20:36:53 +0100

    Create reroll.js

---
+++ VYRN-BOT/GIVEAWAY-BOT/commands/reroll.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { reroll } = require("../utils/giveawaySystem");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("reroll")
+    .setDescription("Reroll giveaway winner")
+    .addStringOption(opt =>
+      opt.setName("messageid").setDescription("Giveaway message ID").setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  async execute(interaction, client) {
+    const id = interaction.options.getString("messageid");
+
+    const result = await reroll(client, id);
+
+    interaction.reply({
+      content: `🎉 New winner: ${result}`
+    });
+  }
+};
