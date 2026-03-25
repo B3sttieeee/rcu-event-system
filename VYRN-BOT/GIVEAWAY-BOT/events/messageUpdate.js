d95425690dda38e85d3ec0e6d32331a14294313d
B3sttieeee <andrzejbraun67@gmail.com>
2026-03-25 02:06:14 +0100

    Update messageUpdate.js

--- VYRN-BOT/GIVEAWAY-BOT/events/messageUpdate.js
+++ VYRN-BOT/GIVEAWAY-BOT/events/messageUpdate.js
@@ -1,17 +1,29 @@
+const { EmbedBuilder } = require("discord.js");
+
 module.exports = {
   name: "messageUpdate",
 
   async execute(oldMsg, newMsg) {
     if (!oldMsg.guild || oldMsg.author?.bot) return;
-
     if (oldMsg.content === newMsg.content) return;
 
     const ch = oldMsg.guild.channels.cache.get("1475992778554216448");
-
     if (!ch) return;
 
-    ch.send(
-      `✏️ ${oldMsg.author.tag}\nOLD: ${oldMsg.content}\nNEW: ${newMsg.content}`
-    );
+    const embed = new EmbedBuilder()
+      .setColor("Blue")
+      .setAuthor({
+        name: oldMsg.author.tag,
+        iconURL: oldMsg.author.displayAvatarURL()
+      })
+      .setTitle("✏️ Message Edited")
+      .addFields(
+        { name: "Before", value: oldMsg.content || "brak", inline: false },
+        { name: "After", value: newMsg.content || "brak", inline: false }
+      )
+      .setFooter({ text: `User ID: ${oldMsg.author.id}` })
+      .setTimestamp();
+
+    ch.send({ embeds: [embed] });
   }
 };
