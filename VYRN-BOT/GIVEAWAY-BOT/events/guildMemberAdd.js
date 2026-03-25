da40af10b039b2b2826bfae6abd1c9546d626010
B3sttieeee <andrzejbraun67@gmail.com>
2026-03-25 01:44:45 +0100

    Update guildMemberAdd.js

--- VYRN-BOT/GIVEAWAY-BOT/events/guildMemberAdd.js
+++ VYRN-BOT/GIVEAWAY-BOT/events/guildMemberAdd.js
@@ -5,17 +5,30 @@ const AUTO_ROLE = "1475572275095929022";
 
 module.exports = {
   name: "guildMemberAdd",
-  async execute(member) {
 
+  async execute(member) {
     try {
+
+      console.log(`👤 New member: ${member.user.tag}`);
 
       // ===== AUTO ROLE =====
       const role = member.guild.roles.cache.get(AUTO_ROLE);
-      if (role) await member.roles.add(role).catch(() => {});
+
+      if (role) {
+        await member.roles.add(role).catch(err => {
+          console.log("❌ ROLE ERROR:", err);
+        });
+      } else {
+        console.log("❌ Role not found");
+      }
 
       // ===== CHANNEL =====
       const channel = member.guild.channels.cache.get(WELCOME_CHANNEL);
-      if (!channel) return;
+
+      if (!channel) {
+        console.log("❌ Channel not found");
+        return;
+      }
 
       // ===== EMBED =====
       const embed = new EmbedBuilder()
@@ -38,14 +51,16 @@ module.exports = {
 
 🔥 Good Luck!`
         )
+        .setThumbnail(member.user.displayAvatarURL())
         .setImage("https://media.discordapp.net/attachments/1475993508535074816/1476584792048013312/Fallen-Knight-in-Burning-Forest.gif")
         .setFooter({ text: "Administrations | VYRN" });
 
-      channel.send({ embeds: [embed] });
+      await channel.send({ embeds: [embed] });
+
+      console.log("✅ Welcome sent");
 
     } catch (err) {
       console.log("❌ WELCOME ERROR:", err);
     }
-
   }
 };
