0e35b62839ca816c6396411e795c161c1f728c5e
B3sttieeee <andrzejbraun67@gmail.com>
2026-03-25 20:53:39 +0100

    Update giveaway.js

--- VYRN-BOT/GIVEAWAY-BOT/commands/giveaway.js
+++ VYRN-BOT/GIVEAWAY-BOT/commands/giveaway.js
@@ -1,67 +1,54 @@
 const {
   SlashCommandBuilder,
-  EmbedBuilder,
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle
+  PermissionFlagsBits
 } = require("discord.js");
 
-let giveaways = {};
+const { createGiveaway } = require("../utils/giveawaySystem");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("giveaway")
     .setDescription("Create giveaway")
-    .addStringOption(o =>
-      o.setName("nagroda").setDescription("Nagroda").setRequired(true))
-    .addIntegerOption(o =>
-      o.setName("czas").setDescription("Minuty").setRequired(true))
-    .addRoleOption(o =>
-      o.setName("bonus").setDescription("Bonus role")),
 
-  async execute(interaction) {
-    const prize = interaction.options.getString("nagroda");
-    const time = interaction.options.getInteger("czas");
-    const bonusRole = interaction.options.getRole("bonus");
+    .addStringOption(opt =>
+      opt.setName("prize").setDescription("Prize").setRequired(true)
+    )
+    .addStringOption(opt =>
+      opt.setName("time").setDescription("Time (10m, 1h)").setRequired(true)
+    )
+    .addIntegerOption(opt =>
+      opt.setName("winners").setDescription("Winners").setRequired(true)
+    )
+    .addChannelOption(opt =>
+      opt.setName("channel").setDescription("Channel").setRequired(true)
+    )
+    .addStringOption(opt =>
+      opt.setName("image").setDescription("Image URL").setRequired(false)
+    )
 
-    const embed = new EmbedBuilder()
-      .setColor("#ff6600")
-      .setTitle("🎉 Giveaway")
-      .setDescription(
-        `🎁 ${prize}\n\nKliknij Join aby wziąć udział\n\n👥 0\n🏆 1\n⏳ ${time} min`
-      );
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
-    const row = new ActionRowBuilder().addComponents(
-      new ButtonBuilder()
-        .setCustomId("join")
-        .setLabel("Join")
-        .setStyle(ButtonStyle.Success),
-      new ButtonBuilder()
-        .setCustomId("leave")
-        .setLabel("Leave")
-        .setStyle(ButtonStyle.Secondary)
-    );
+  async execute(interaction, client) {
 
-    const msg = await interaction.reply({
-      embeds: [embed],
-      components: [row],
-      fetchReply: true
+    const prize = interaction.options.getString("prize");
+    const time = interaction.options.getString("time");
+    const winners = interaction.options.getInteger("winners");
+    const channel = interaction.options.getChannel("channel");
+    const image = interaction.options.getString("image");
+
+    await createGiveaway({
+      client,
+      channel,
+      host: interaction.user,
+      prize,
+      duration: time,
+      winners,
+      image
     });
 
-    giveaways[msg.id] = {
-      users: [],
-      bonusRole,
-      prize
-    };
-
-    setTimeout(() => {
-      const data = giveaways[msg.id];
-      if (!data || data.users.length === 0) return;
-
-      const winner =
-        data.users[Math.floor(Math.random() * data.users.length)];
-
-      interaction.channel.send(`🏆 Winner: <@${winner}> (${prize})`);
-    }, time * 60000);
+    interaction.reply({
+      content: "✅ Giveaway created",
+      ephemeral: true
+    });
   }
 };
