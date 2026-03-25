b537339fe7466d06841d82852b3f9f5e08886d03
B3sttieeee <andrzejbraun67@gmail.com>
2026-03-25 02:05:56 +0100

    Update guildMemberRemove.js

--- VYRN-BOT/GIVEAWAY-BOT/events/guildMemberRemove.js
+++ VYRN-BOT/GIVEAWAY-BOT/events/guildMemberRemove.js
@@ -1,11 +1,24 @@
+const { EmbedBuilder } = require("discord.js");
+
 module.exports = {
   name: "guildMemberRemove",
 
   async execute(member) {
-    const channel = member.guild.channels.cache.get("1475992846912721018");
+    const ch = member.guild.channels.cache.get("1475992846912721018");
 
-    if (channel) {
-      channel.send(`➖ ${member.user.tag} left`);
-    }
+    if (!ch) return;
+
+    const embed = new EmbedBuilder()
+      .setColor("Red")
+      .setAuthor({
+        name: "Member Left",
+        iconURL: member.user.displayAvatarURL()
+      })
+      .setDescription(
+        `${member.user.tag}\n\n🆔 ID: ${member.id}`
+      )
+      .setTimestamp();
+
+    ch.send({ embeds: [embed] });
   }
 };
