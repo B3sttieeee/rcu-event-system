7ff4fe6db52110d7341cadd1b0371a8f1f992efc
B3sttieeee <andrzejbraun67@gmail.com>
2026-03-27 18:17:05 +0100

    Update reroll.js

--- VYRN-BOT/GIVEAWAY-BOT/commands/reroll.js
+++ VYRN-BOT/GIVEAWAY-BOT/commands/reroll.js
@@ -6,16 +6,21 @@ module.exports = {
     .setName("reroll")
     .setDescription("Reroll giveaway winner")
     .addStringOption(opt =>
-      opt.setName("messageid").setDescription("Giveaway message ID").setRequired(true)
+      opt.setName("messageid")
+        .setDescription("Giveaway message ID")
+        .setRequired(true)
     )
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
   async execute(interaction, client) {
+
+    await interaction.deferReply(); // 🔥 FIX
+
     const id = interaction.options.getString("messageid");
 
     const result = await reroll(client, id);
 
-    interaction.reply({
+    await interaction.editReply({
       content: `🎉 New winner: ${result}`
     });
   }
