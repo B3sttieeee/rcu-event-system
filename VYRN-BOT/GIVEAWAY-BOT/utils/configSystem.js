c24af88d3aece818b4c196e2e697e940085b19c2
B3sttieeee <andrzejbraun67@gmail.com>
2026-03-29 14:21:27 +0200

    Create configSystem.js

---
+++ VYRN-BOT/GIVEAWAY-BOT/utils/configSystem.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "../data/config.json");
+
+function load() {
+  if (!fs.existsSync(filePath)) return {};
+  return JSON.parse(fs.readFileSync(filePath));
+}
+
+function save(data) {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+}
+
+function getConfig(guildId) {
+  const data = load();
+  return data[guildId] || {};
+}
+
+function setConfig(guildId, key, value) {
+  const data = load();
+  if (!data[guildId]) data[guildId] = {};
+
+  data[guildId][key] = value;
+  save(data);
+}
+
+module.exports = {
+  getConfig,
+  setConfig
+};
