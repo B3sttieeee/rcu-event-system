2ab40249d60dde7f2a4c6bd67478cbccb61468e0
B3sttieeee <andrzejbraun67@gmail.com>
2026-04-07 22:03:21 +0200

    Update balance.js

--- VYRN-BOT/GIVEAWAY-BOT/commands/economy/balance.js
+++ VYRN-BOT/GIVEAWAY-BOT/commands/economy/balance.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const { getCoins } = require("../../../utils/economySystem");   // ← POPRAWIONA ŚCIEŻKA
+const { getCoins } = require("../../utils/economySystem");
 
 module.exports = {
   data: new SlashCommandBuilder()
