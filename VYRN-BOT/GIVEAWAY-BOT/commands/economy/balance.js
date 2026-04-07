739bdb7a9cb40bae84831e944b2c4017f489847e
B3sttieeee <andrzejbraun67@gmail.com>
2026-04-07 23:15:52 +0200

    Update balance.js

--- VYRN-BOT/GIVEAWAY-BOT/commands/economy/balance.js
+++ VYRN-BOT/GIVEAWAY-BOT/commands/economy/balance.js
@@ -18,7 +18,10 @@ module.exports = {
         .addFields(
           {
             name: "━━━━━━━━━━━━━━━━━━━━━━",
-            value: "Użyj `/shop`, aby przejrzeć dostępne boosty",
+            value: "**Jak zdobywać monety?**\n" +
+                   "• Pisanie wiadomości → **3** <:CASHH:1491180511308157041> co 12 sekund\n" +
+                   "• Spędzanie czasu na VC → **8** <:CASHH:1491180511308157041> za minutę\n" +
+                   "• Wbijanie poziomów → **+50** <:CASHH:1491180511308157041>",
             inline: false
           }
         )
