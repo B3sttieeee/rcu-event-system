d1bb0e36bb655e41fe6aa78666bcb74be3728103
B3sttieeee <andrzejbraun67@gmail.com>
2026-04-08 15:19:31 +0200

    Update embed.js

--- VYRN-BOT/GIVEAWAY-BOT/commands/embed.js
+++ VYRN-BOT/GIVEAWAY-BOT/commands/embed.js
@@ -14,7 +14,7 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .addChannelOption(opt =>
       opt.setName('kanal')
-        .setDescription('Kanał docelowy (opcjonalnie)')
+        .setDescription('Kanał docelowy')
         .setRequired(false)
     ),
 
@@ -22,7 +22,7 @@ module.exports = {
     try {
       const targetChannel = interaction.options.getChannel('kanal') || interaction.channel;
 
-      // Pobieramy embed z wiadomości, na której kliknięto przycisk "Edytuj" (jeśli istnieje)
+      // Pobieramy embed z wiadomości, jeśli kliknięto "Edytuj"
       let oldEmbed = null;
       if (interaction.message && interaction.message.embeds.length > 0) {
         oldEmbed = interaction.message.embeds[0];
@@ -32,13 +32,15 @@ module.exports = {
         .setCustomId(`embedModal_${targetChannel.id}`)
         .setTitle('📦 Embed Builder');
 
+      // TITLE
       const titleInput = new TextInputBuilder()
         .setCustomId('title')
-        .setLabel('Tytuł embeda')
+        .setLabel('Tytuł')
         .setStyle(TextInputStyle.Short)
         .setValue(oldEmbed?.title || "")
         .setRequired(false);
 
+      // DESCRIPTION
       const descriptionInput = new TextInputBuilder()
         .setCustomId('description')
         .setLabel('Opis embeda')
@@ -47,31 +49,37 @@ module.exports = {
         .setRequired(false)
         .setPlaceholder('Wpisz "." jeśli nie chcesz opisu');
 
+      // COLOR
       const colorInput = new TextInputBuilder()
         .setCustomId('color')
-        .setLabel('Kolor HEX (np. #ff0000)')
+        .setLabel('Kolor HEX (#ff0000)')
         .setStyle(TextInputStyle.Short)
         .setValue(oldEmbed?.hexColor ? `#${oldEmbed.hexColor}` : "#2b2d31")
         .setRequired(false);
 
+      // AUTHOR
+      let authorValue = "";
+      if (oldEmbed?.author) {
+        authorValue = oldEmbed.author.iconURL 
+          ? `${oldEmbed.author.name} | ${oldEmbed.author.iconURL}`
+          : oldEmbed.author.name;
+      }
       const authorInput = new TextInputBuilder()
         .setCustomId('author')
         .setLabel('Autor (nazwa | icon URL)')
         .setStyle(TextInputStyle.Short)
-        .setValue(oldEmbed?.author ? 
-          (oldEmbed.author.iconURL ? 
-            `${oldEmbed.author.name} | ${oldEmbed.author.iconURL}` : 
-            oldEmbed.author.name) : "")
+        .setValue(authorValue)
         .setRequired(false)
         .setPlaceholder('Admin | https://i.imgur.com/...');
 
+      // IMAGE / GIF
       const imageInput = new TextInputBuilder()
         .setCustomId('image')
-        .setLabel('URL obrazka / GIF')
+        .setLabel('URL obrazka lub GIF')
         .setStyle(TextInputStyle.Short)
         .setValue(oldEmbed?.image?.url || "")
         .setRequired(false)
-        .setPlaceholder('https://i.imgur.com/...');
+        .setPlaceholder('https://i.imgur.com/...gif');
 
       modal.addComponents(
         new ActionRowBuilder().addComponents(titleInput),
