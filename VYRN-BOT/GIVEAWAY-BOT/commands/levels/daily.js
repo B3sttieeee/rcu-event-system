b018984b8f3b10a8e4f9a22236a3d2709ff4a3f7
B3sttieeee <andrzejbraun67@gmail.com>
2026-04-22 11:52:24 +0200

    Update daily.js

--- VYRN-BOT/GIVEAWAY-BOT/commands/levels/daily.js
+++ VYRN-BOT/GIVEAWAY-BOT/commands/levels/daily.js
@@ -4,20 +4,18 @@ const { buildDailyEmbed } = require("../../utils/dailySystem");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("daily")
-    .setDescription("🎯 Sprawdź postęp i odbierz Daily Quest"),
+    .setDescription("🌙 Sprawdź postęp i odbierz Daily Quest"),
 
   async execute(interaction) {
     try {
       await interaction.deferReply({ ephemeral: true });
 
-      const userId = interaction.user.id;
-      const { embed, components } = buildDailyEmbed(userId);
+      const { embed, components } = buildDailyEmbed(interaction.user.id);
 
       await interaction.editReply({
         embeds: [embed],
         components: components || []
       });
-
     } catch (err) {
       console.error("[DAILY CMD] Błąd:", err);
       await interaction.editReply({
