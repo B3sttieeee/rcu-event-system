1a7d0bedbb83ef43ee7205281fa8861b7e37ad9d
B3sttieeee <andrzejbraun67@gmail.com>
2026-04-23 02:29:47 +0200

    Delete VYRN-BOT/GIVEAWAY-BOT directory

--- VYRN-BOT/GIVEAWAY-BOT/models/Panel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const panelSchema = new mongoose.Schema({
-  guildId: String,
-  messageId: String
-});
-
-module.exports = mongoose.model('Panel', panelSchema);
