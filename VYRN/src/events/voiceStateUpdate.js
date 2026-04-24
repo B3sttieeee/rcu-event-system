6601625e1199ad9fd066e88242ea8c229dd2c63e
B3sttieeee <andrzejbraun67@gmail.com>
2026-04-24 16:12:45 +0200

    Update voiceStateUpdate.js

--- VYRN/src/events/voiceStateUpdate.js
+++ VYRN/src/events/voiceStateUpdate.js
@@ -7,6 +7,7 @@ const voiceRewards = require("../systems/voiceRewards");
 
 module.exports = {
   name: Events.VoiceStateUpdate,
+
   async execute(oldState, newState) {
     try {
       const member = newState.member || oldState.member;
@@ -17,17 +18,20 @@ module.exports = {
 
       // === PRIVATE VC CREATION ===
       if (!oldChannel && newChannel && newChannel === "1496280414237491220") {
+        console.log(`[PRIVATE VC] Trigger create for ${member.user.tag}`);
         await privateVC.handlePrivateChannelCreation(member);
         return;
       }
 
-      // === NORMAL VOICE REWARDS ===
+      // === NORMAL VOICE REWARDS (XP + Coins + Voice Time) ===
       if (!oldChannel && newChannel) {
         voiceRewards.startSession(member);
-      } else if (oldChannel && !newChannel) {
+      } 
+      else if (oldChannel && !newChannel) {
         voiceRewards.stopSession(member.id);
         console.log(`[VOICE LEAVE] ${member.user.tag}`);
-      } else if (oldChannel && newChannel && oldChannel !== newChannel) {
+      } 
+      else if (oldChannel && newChannel && oldChannel !== newChannel) {
         voiceRewards.stopSession(member.id);
         voiceRewards.startSession(member);
         console.log(`[VOICE SWITCH] ${member.user.tag}`);
